192284dd8b1ab901742f5f50e641920986c529e8
zhangyuexin <zhangyuexin888@163.com>
2019-07-13 14:22:35 +0800

    初始化项目

---
+++ src/main/java/cn/itcast/Demo.java
@@ -0,0 +1,14 @@
+package cn.itcast;
+
+/**
+ * @program: github_test
+ * @description:
+ * @author: ZhangYuexin
+ * @date : Created in 2019/7/13 14:15
+ * @tips:
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("初始化");
+    }
+}
